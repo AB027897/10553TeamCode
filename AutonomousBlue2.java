48931c9c85c4bcf7740a2391162badf4d900969c
AB027897 <47727880+AB027897@users.noreply.github.com>
2021-04-14 21:04:01 -0400

    Update AutonomousBlue2.java

--- AutonomousBlue2.java
+++ AutonomousBlue2.java
@@ -1,4 +1,4 @@
-                 package org.firstinspires.ftc.teamcode.ftc16072;
+package org.firstinspires.ftc.teamcode.ftc10553;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
