3f0ae5d65e289b0f3440d14730387a56b92f7fdf
Adam <AB027897@students.nutleyschools.org>
2021-04-21 21:57:32 -0400

    Update TeleOp2.java

--- TeleOp2.java
+++ TeleOp2.java
@@ -9,7 +9,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
-// Sets the preview name on the simulator
+// Sets the preview name on the robot controller
 @TeleOp(name="TeleOp2")
 
 public class TeleOp2 extends LinearOpMode {
@@ -28,8 +28,7 @@ public class TeleOp2 extends LinearOpMode {
    private Servo feeder = null;
    private CRServo intake = null;
    double position = 0;
-   final double servoPower = 0.3;
-   final double shootingPower = 1;
+   boolean set = false;;
    @Override
    public void runOpMode() {
        telemetry.addData("Status:", "TeleOp Initialized");
@@ -64,36 +63,47 @@ public class TeleOp2 extends LinearOpMode {
            double rightFrontPower;
            double leftBackPower;
            double rightBackPower;
-           double liftPower; 
-           double 
+           double intakePower;
+           double feederPos;
+           double feederNeg;
            //Takes input from gamepads and assigns it to the variables attached to motor power
            double drive = -gamepad1.left_stick_y;
            double turn  = gamepad1.left_stick_x;
            double strafe = gamepad1.right_stick_x;
-           double lift  = gamepad2.left_stick_y;
+           double liftPower  = gamepad2.left_stick_y;
            double shoot = gamepad2.right_stick_y; 
-           double intake= gamepad2.left_trigger;
+           double intakePower= gamepad2.left_trigger;
            double reverseIntake = -gamepad2.right_trigger;
-           //Takes input from gamepad2 buttons and sets the power for the scooper servos and the shooting servos.
-           if(gamepad2.left_bumper) {
-               scoop_left.setPower(servoPower);
-               scoop_right.setPower(-servoPower);
-           } else if(gamepad2.right_bumper) {
-               scoop_left.setPower(-servoPower);
-               scoop_right.setPower(servoPower);
-           } 
+           if(gamepad2.a) {
+               if(set) {
+                set= false;
+                feederPos = 1;
+                
+               } else {
+                set = true;
+                feederNeg = 1;
+               }
+           }
            // Has power amounts respond to joystick and makes sure the power isn’t over the maximum input of a motor
            leftFrontPower    = Range.clip(drive + strafe + turn, -1.0, 1.0) ;
            rightFrontPower   = Range.clip(drive - strafe - turn, -1.0, 1.0) ;
            leftBackPower = Range.clip(drive - strafe + turn , -1.0, 1.0);
            rightBackPower = Range.clip(drive + strafe - turn , -1.0, 1.0);
-           //position+= (aimPos + aimNeg)*0.1;
+           intakePower = Range.clip(intake + reverseIntake, -1.0, 1.0);
+           position+= (aimPos + aimNeg)*0.25;
            // Has motors respond to input values.
            front_left_motor.setPower(leftFrontPower);
            front_right_motor.setPower(rightFrontPower);
            back_left_motor.setPower(leftBackPower);
            back_right_motor.setPower(rightBackPower);
-           //Regular servos move to position.
+           lift.setPower(liftPower);
+           pulley.setPower();
+           shooter_left.setPower(shoot);
+           shooter_right.setPower(shoot);
+           //Set continous servo power
+           intake.setPower(intakePower);
+           //Regular servo move to position.
+           feeder.setPosition(position);
            // Shows the power that the motors are running at as well as the time on the telemetry
            telemetry.addData("Status:", "Run Time: " + runtime.toString());
            telemetry.addData("Current Motor Powers", "front left (%.2f), front right (%.2f), back left (%.2f), back right (%.2f)", leftFrontPower, rightFrontPower, leftBackPower, rightBackPower);
